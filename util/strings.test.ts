0de331557f83ce3205567e453d5c4dd3cb6dedef
tadashi-aikawa <syou.maman@gmail.com>
2024-11-10 18:50:39 +0900

    feat: pickPatterns と parseMarkdownList を追加

--- util/strings.test.ts
+++ util/strings.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "@std/assert/equals";
 import { parameterizedTest } from "../tests/testUtil.ts";
-import { match } from "./strings.ts";
+import { match, parseMarkdownList, pickPatterns } from "./strings.ts";
 
 parameterizedTest(
   "match",
@@ -13,5 +13,72 @@ parameterizedTest(
   ] as const,
   ([text, pattern, expected]) => {
     assertEquals(match(text, pattern), expected);
+  },
+);
+
+parameterizedTest<
+  [
+    Parameters<typeof pickPatterns>[0],
+    Parameters<typeof pickPatterns>[1],
+    ReturnType<typeof pickPatterns>,
+  ]
+>(
+  "function",
+  // deno-fmt-ignore
+  // prettier-ignore
+  [
+    ["- hoge hoge", /^- (?<name>.+)/g                      , { name: "hoge hoge" }],
+    ["- 1ro 2ro"  , /^- (?<first>[^ ]+) (?<second>[^ ]+)/g , {first: "1ro", second: "2ro"}],
+    ["* hoge hoge", /^- (?<name>.+)/g                      , {}],
+  ] as const,
+  ([str, pattern, expected]) => {
+    assertEquals(pickPatterns(str, pattern), expected);
+  },
+);
+
+parameterizedTest<
+  [
+    Parameters<typeof parseMarkdownList>[0],
+    ReturnType<typeof parseMarkdownList>,
+  ]
+>(
+  "function",
+  // deno-fmt-ignore
+  // prettier-ignore
+  [
+    [""              , { prefix: "", content: "" }],
+    ["hoge"          , { prefix: "", content: "hoge" }],
+    ["- "            , { prefix: "- ", content: "" }],
+    ["- hoge"        , { prefix: "- ", content: "hoge" }],
+    ["- [ ] hoge"    , { prefix: "- [ ] ", content: "hoge" }],
+    ["- [x] hoge"    , { prefix: "- [x] ", content: "hoge" }],
+    ["  hoge"        , { prefix: "  ", content: "hoge" }],
+    ["  - "          , { prefix: "  - ", content: "" }],
+    ["  - hoge"      , { prefix: "  - ", content: "hoge" }],
+    ["  - [ ] hoge"  , { prefix: "  - [ ] ", content: "hoge" }],
+    ["  - [x] hoge"  , { prefix: "  - [x] ", content: "hoge" }],
+    ["* "            , { prefix: "* ", content: "" }],
+    ["* hoge"        , { prefix: "* ", content: "hoge" }],
+    ["* [ ] hoge"    , { prefix: "* [ ] ", content: "hoge" }],
+    ["* [x] hoge"    , { prefix: "* [x] ", content: "hoge" }],
+    ["\t- "          , { prefix: "\t- ", content: "" }],
+    ["\t- hoge"      , { prefix: "\t- ", content: "hoge" }],
+    ["\t- [ ] hoge"  , { prefix: "\t- [ ] ", content: "hoge" }],
+    ["\t- [x] hoge"  , { prefix: "\t- [x] ", content: "hoge" }],
+    ["\t\t- "        , { prefix: "\t\t- ", content: "" }],
+    ["\t\t- hoge"    , { prefix: "\t\t- ", content: "hoge" }],
+    ["\t\t- [ ] hoge", { prefix: "\t\t- [ ] ", content: "hoge" }],
+    ["\t\t- [x] hoge", { prefix: "\t\t- [x] ", content: "hoge" }],
+    ["　- "          , { prefix: "　- ", content: "" }],
+    ["　- hoge"      , { prefix: "　- ", content: "hoge" }],
+    ["　- [ ] hoge"  , { prefix: "　- [ ] ", content: "hoge" }],
+    ["　- [x] hoge"  , { prefix: "　- [x] ", content: "hoge" }],
+    ["　　- "        , { prefix: "　　- ", content: "" }],
+    ["　　- hoge"    , { prefix: "　　- ", content: "hoge" }],
+    ["　　- [ ] hoge", { prefix: "　　- [ ] ", content: "hoge" }],
+    ["　　- [x] hoge", { prefix: "　　- [x] ", content: "hoge" }],
+  ] as const,
+  ([text, expected]) => {
+    assertEquals(parseMarkdownList(text), expected);
   },
 );
--- util/strings.ts
+++ util/strings.ts
@@ -4,3 +4,30 @@
 export function match(text: string, pattern: RegExp): boolean {
   return pattern.exec(text)?.[0] === text;
 }
+
+/**
+ * マッチした正規表現キャプチャグループを抽出する
+ */
+export function pickPatterns(
+  str: string,
+  pattern: RegExp,
+): { [key: string]: string } {
+  return Array.from(str.matchAll(pattern))?.[0]?.groups ?? {};
+}
+
+/**
+ * Markdownのリスト1行からコンテンツと手前部分(prefix)を切り離します
+ */
+export function parseMarkdownList(text: string): {
+  prefix: string;
+  content: string;
+} {
+  const { prefix, content } = pickPatterns(
+    text,
+    /^(?<prefix>[ \t\s]*([-*] (\[.] |)|))(?<content>.*)$/g,
+  );
+  if (prefix == null || content == null) {
+    throw Error("parseMarkdownList doesn't return a correct object");
+  }
+  return { prefix, content };
+}
